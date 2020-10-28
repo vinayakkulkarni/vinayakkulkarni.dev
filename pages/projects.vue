152bf22a8a6336dd489ff3d0cacd7d4bfc762f50
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-10-29 01:20:39 +0530

    feat: add projects as array

--- pages/projects.vue
+++ pages/projects.vue
@@ -18,17 +18,19 @@
     </div>
     <div class="grid gap-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
       <div
+        v-for="(project, idx) in projects"
+        :key="idx"
         class="overflow-hidden text-gray-100 rounded shadow md:max-w-lg hover:shadow-md bg-gradient-to-tr from-cool-gray-700 to-cool-gray-600 hover:bg-gradient-to-bl hover:from-cool-gray-600 hover:to-cool-gray-700"
       >
         <div class="p-4">
           <div class="mb-2 text-xl font-bold">
             <a
-              href="https://github.com/vinayakkulkarni/janak"
+              :href="project.url"
               target="_blank"
               rel="noopener noreferrer"
               class="flex items-center"
             >
-              <span>Janak</span>
+              <span>{{ project.name }}</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 aria-hidden="true"
@@ -50,117 +52,20 @@
               </svg>
             </a>
           </div>
-          <p class="text-base">Build Vue 3 apps in a breeze!</p>
-        </div>
-      </div>
-      <div
-        class="overflow-hidden text-gray-100 rounded shadow md:max-w-lg hover:shadow-md bg-gradient-to-tr from-cool-gray-700 to-cool-gray-600 hover:bg-gradient-to-bl hover:from-cool-gray-600 hover:to-cool-gray-700"
-      >
-        <div class="p-4">
-          <div class="mb-2 text-xl font-bold">
-            <a
-              href="https://github.com/vinayakkulkarni/v-offline"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex items-center"
-            >
-              <span>v-offline</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                x="0px"
-                y="0px"
-                viewBox="0 0 100 100"
-                width="15"
-                height="15"
-                class="w-4 ml-1 fill-current"
-              >
-                <path
-                  fill="currentColor"
-                  d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-                />
-                <polygon
-                  fill="currentColor"
-                  points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-                />
-              </svg>
-            </a>
-          </div>
-          <p class="text-base">Detect Offline & Online events</p>
-        </div>
-      </div>
-      <div
-        class="overflow-hidden text-gray-100 rounded shadow md:max-w-lg hover:shadow-md bg-gradient-to-tr from-cool-gray-700 to-cool-gray-600 hover:bg-gradient-to-bl hover:from-cool-gray-600 hover:to-cool-gray-700"
-      >
-        <div class="p-4">
-          <div class="mb-2 text-xl font-bold">
-            <a
-              href="https://github.com/vinayakkulkarni/v-image"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex items-center"
-            >
-              <span>v-image</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                x="0px"
-                y="0px"
-                viewBox="0 0 100 100"
-                width="15"
-                height="15"
-                class="w-4 ml-1 fill-current"
-              >
-                <path
-                  fill="currentColor"
-                  d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-                />
-                <polygon
-                  fill="currentColor"
-                  points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-                />
-              </svg>
-            </a>
-          </div>
-          <p class="text-base">
-            Tiny css-free component for your input type file
+          <p class="text-base break-words">
+            {{ project.excerpt }}
           </p>
-        </div>
-      </div>
-      <div
-        class="overflow-hidden text-gray-100 rounded shadow md:max-w-lg hover:shadow-md bg-gradient-to-tr from-cool-gray-700 to-cool-gray-600 hover:bg-gradient-to-bl hover:from-cool-gray-600 hover:to-cool-gray-700"
-      >
-        <div class="p-4">
-          <div class="mb-2 text-xl font-bold">
-            <a
-              href="https://github.com/vinayakkulkarni/vue-identify-network"
-              target="_blank"
-              rel="noopener noreferrer"
-              class="flex items-center"
+          <div class="mt-2">
+            <span
+              class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4"
+              :class="{
+                'bg-green-100 text-green-800': project.library === 'vue',
+                'bg-red-100 text-red-800': project.library === 'svelte',
+              }"
             >
-              <span>Vue Identify Network</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                x="0px"
-                y="0px"
-                viewBox="0 0 100 100"
-                width="15"
-                height="15"
-                class="w-4 ml-1 fill-current"
-              >
-                <path
-                  fill="currentColor"
-                  d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"
-                />
-                <polygon
-                  fill="currentColor"
-                  points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"
-                />
-              </svg>
-            </a>
+              {{ project.library }}
+            </span>
           </div>
-          <p class="text-base">Detect Offline & Online events</p>
         </div>
       </div>
     </div>
@@ -175,6 +80,78 @@
         return 'slide-right';
       }
       return 'slide-left';
+    },
+    data() {
+      return {
+        projects: [
+          {
+            name: 'Janak',
+            excerpt: '🤙 Build Vue 3 apps in a breeze!',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/janak',
+          },
+          {
+            name: 'v-offline',
+            excerpt: '⚡️ Easily detect offline & online events',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/v-offline',
+          },
+          {
+            name: 'v-image',
+            excerpt: '🖼 Tiny css-free component for your input type file',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/v-image',
+          },
+          {
+            name: 'v-intl',
+            excerpt: '🔉 Add i18n to your awesome Vue 3 apps',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/v-intl',
+          },
+          {
+            name: 'v-pip',
+            excerpt: '🌁 Vue wrapper for native picture-in-picture',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/v-intl',
+          },
+          {
+            name: 'v-clappr',
+            excerpt: '📹 Vue wrapper for Clappr media player',
+            url: 'https://github.com/vinayakkulkarni/v-clappr',
+            library: 'vue',
+          },
+          {
+            name: 'v-spotlight',
+            excerpt: '🔦 Tiny reusable spotlight Vue component',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/v-spotlight',
+          },
+          {
+            name: 'v-rating',
+            excerpt: '⭐ Rating component in Semantic-UI',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/v-rating',
+          },
+          {
+            name: 'v-github-icon',
+            excerpt: '🐙 GitHub corner icons',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/v-github-icon',
+          },
+          {
+            name: 's-offline',
+            excerpt: '🎉 Detect offline & online events in Svelte',
+            url: 'https://github.com/vinayakkulkarni/s-offline',
+            library: 'svelte',
+          },
+          {
+            name: 'Vue Identify Network',
+            excerpt: '🚨 Identify network type & speed',
+            library: 'vue',
+            url: 'https://github.com/vinayakkulkarni/vue-identify-network',
+          },
+        ],
+      };
     },
   };
 </script>
