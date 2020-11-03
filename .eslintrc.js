096c9dfcc213105b698dc93442be779154890d5d
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-03 17:28:33 +0530

    style: update `.eslintrc` to use typescript parser

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,15 @@ module.exports = {
   root: true,
   env: {
     browser: true,
+    es6: true,
     node: true,
   },
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@typescript-eslint/parser',
   },
   extends: [
     '@nuxtjs',
+    '@nuxtjs/eslint-config-typescript',
     'prettier',
     'prettier/vue',
     'plugin:prettier/recommended',
@@ -16,5 +18,7 @@ module.exports = {
   ],
   plugins: ['prettier', 'vue'],
   // add your custom rules here
-  rules: {},
+  rules: {
+    'nuxt/no-cjs-in-config': 'off',
+  },
 };
