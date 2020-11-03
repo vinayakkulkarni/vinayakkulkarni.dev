203bf69af76f0ee49eafd8b5b58ae639d04e7192
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2020-11-03 17:29:33 +0530

    feat(ts): update code for better intellisense

---
+++ types/blog.ts
@@ -0,0 +1,16 @@
+export interface Post {
+  body: object;
+  category: string;
+  createdAt: string;
+  description: string;
+  dir: string;
+  extension: string;
+  path: string;
+  position: number;
+  slug: string;
+  status: string;
+  tags: [];
+  title: string;
+  toc: [];
+  updatedAt: string;
+}
