ef3445227ad030c533fb4ef64da23ab559a884cc
Vinayak Kulkarni <inbox.vinayak@gmail.com>
2021-01-10 18:16:00 +0530

    refactor: update tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,21 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-  experimental: {
-    darkModeVariant: true,
-  },
-  dark: 'class',
-  future: {
-    defaultLineHeights: true,
-    standardFontWeights: true,
-    removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true,
-  },
+  darkMode: 'class',
+  prefix: '',
+  separator: ':',
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+  ],
   theme: {
     extend: {
       colors: {
@@ -22,24 +27,138 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            color: theme('colors.gray.800'),
+            a: {
+              color: theme('colors.blue.500'),
+              'text-decoration': 'none',
+              '&:hover, &.active': {
+                color: 'white',
+                'background-color': theme('colors.blue.500'),
+                strong: {
+                  color: 'white',
+                },
+              },
+            },
+            strong: {
+              color: theme('colors.blue.500'),
+            },
+            h1: {
+              color: theme('colors.gray.800'),
+              'margin-top': '0',
+            },
+            h2: {
+              color: theme('colors.gray.800'),
+              'margin-top': '0',
+            },
+            h3: {
+              color: theme('colors.gray.800'),
+              'margin-top': '0',
+            },
+            h4: {
+              color: theme('colors.gray.800'),
+              'margin-top': '0',
+            },
+            code: {
+              color: 'white',
+              'background-color': theme('colors.gray.800'),
+              '&:before, &:after': {
+                display: 'none',
+              },
+            },
+            p: {
+              color: theme('colors.gray.800'),
+              'margin-top': '0',
+              'margin-bottom': '1em',
+            },
+            img: {
+              'margin-top': '0',
+              'margin-bottom': '0',
+              'box-shadow': '0px 2px 4px -2px rgba(0, 0, 0, 30%)',
+            },
+            'ul > li': {
+              '&::before': {
+                'background-color': theme('colors.gray.800'),
+                'font-weight': 'bold',
+              },
+            },
+            'ol > li': {
+              '&::before': {
+                color: theme('colors.gray.800'),
+                'font-weight': 'bold',
+              },
+            },
+          },
+        },
+
+        dark: {
+          css: {
+            color: 'white',
+            a: {
+              color: theme('colors.blue.500'),
+              'text-decoration': 'none',
+              '&:hover, &.active': {
+                color: 'white',
+                'background-color': theme('colors.blue.500'),
+              },
+            },
+            strong: {
+              color: theme('colors.blue.500'),
+            },
+            h1: {
+              color: 'white',
+              'margin-top': '0',
+            },
+            h2: {
+              color: 'white',
+              'margin-top': '0',
+            },
+            h3: {
+              color: 'white',
+              'margin-top': '0',
+            },
+            h4: {
+              color: 'white',
+              'margin-top': '0',
+            },
+            code: {
+              color: theme('colors.gray.100'),
+              '&:before, &:after': {
+                display: 'none',
+              },
+            },
+            p: {
+              color: 'white',
+              'margin-top': '0',
+              'margin-bottom': '1em',
+            },
+            img: {
+              'margin-top': '0',
+              'margin-bottom': '0',
+              'box-shadow': '0px 2px 4px -2px rgba(255, 255, 255, 30%)',
+            },
+            'ul > li': {
+              '&::before': {
+                'background-color': 'white',
+                'font-weight': 'bold',
+              },
+            },
+            'ol > li': {
+              '&::before': {
+                color: 'white',
+                'font-weight': 'bold',
+              },
+            },
+          },
+        },
+      }),
     },
-    // typography: (theme) => ({
-    //   dark: {
-    //     css: {
-    //       color: theme('colors.gray.50'),
-    //       a: {
-    //         color: theme('colors.blue.100'),
-    //         '&:hover': {
-    //           color: theme('colors.blue.100'),
-    //         },
-    //       },
-    //     },
-    //   },
-    // }),
   },
-  plugins: [
-    require('@tailwindcss/ui'),
-    require('@tailwindcss/custom-forms'),
-    require('@tailwindcss/typography'),
-  ],
+  variants: {
+    extend: {
+      typography: ['dark'],
+    },
+  },
 };
