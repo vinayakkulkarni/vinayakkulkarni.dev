52bf1c7efdcde1b18e97107898e4863228cea982
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-07-04 23:54:46 +0530

    build: clean up of build for Nuxt 3

--- config/build.ts
+++ config/build.ts
@@ -3,23 +3,4 @@ import type { Configuration } from 'webpack';
 // Build Configuration (https://go.nuxtjs.dev/config-build)
 export const build = {
   corejs: '3',
-  terser: {
-    cache: true,
-    parallel: true,
-    sourceMap: true,
-    terserOptions: {
-      ecma: 2020,
-      mangle: true,
-      module: true,
-      sourceMap: true,
-    },
-  },
-  extend(config: Configuration) {
-    config.devtool = 'source-map';
-    config.module.rules.push({
-      test: /\.mjs$/,
-      include: /node_modules/,
-      type: 'javascript/auto',
-    });
-  },
 };
