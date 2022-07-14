117dd7f5569adcd91f43a5f19dd0e00dea4af3da
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-07-14 12:30:52 +0530

    chore: use doc[0]
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- pages/articles/[article].vue
+++ pages/articles/[article].vue
@@ -1,14 +1,12 @@
 <template>
   <article class="flex flex-col w-full h-full prose prose-lg">
     <content-doc v-slot="{ doc }">
-      <lazy-content-renderer :value="doc" />
-      <!-- <content-doc v-slot="{ doc }"> -->
-      <!-- <h1>{{ doc.title }}</h1>
+      <h1>{{ doc[0].title }}</h1>
       <div
-        v-if="doc.tags && doc.tags.length"
+        v-if="doc[0].tags && doc[0].tags.length"
         class="flex items-center justify-start py-1 text-sm"
       >
-        <div v-for="(tag, index) in doc.tags" :key="index" class="pl-2">
+        <div v-for="(tag, index) in doc[0].tags" :key="index" class="pl-2">
           <span
             class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-red-100 text-gray-800 mr-2"
           >
@@ -16,8 +14,8 @@
           </span>
         </div>
       </div>
-      <h3>{{ doc.description }}</h3>
-      <content-renderer :value="doc" /> -->
+      <h3>{{ doc[0].description }}</h3>
+      <content-renderer :value="doc" />
     </content-doc>
   </article>
 </template>
