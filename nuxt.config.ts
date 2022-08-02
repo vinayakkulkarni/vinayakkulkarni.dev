2d082ec35007b72ebf2deed5c1f058b040c23265
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-08-02 18:38:27 +0530

    fix: issue with netlify
    Ref: https://github.com/nuxt/framework/issues/6204#issuecomment-1201398080
    
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,4 +21,13 @@ export default defineNuxtConfig({
       routes: ['/', '/articles', '/projects', '/uses'],
     },
   },
+  hooks: {
+    'vite:extendConfig'(config, { isServer }) {
+      if (isServer) {
+        // Workaround for netlify issue
+        // https://github.com/nuxt/framework/issues/6204
+        config.build.rollupOptions.output.inlineDynamicImports = true
+      }
+    }
+  },
 });
