b5f22b9de6ab3a34f1e4251dfac2ea581eada679
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-08-12 14:17:49 +0530

    chore: remove workaround for `vite`
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,17 +17,5 @@ export default defineNuxtConfig({
   },
   nitro: {
     preset: 'netlify-builder',
-    prerender: {
-      routes: ['/', '/articles', '/projects', '/uses'],
-    },
-  },
-  hooks: {
-    'vite:extendConfig'(config, { isServer }) {
-      if (isServer) {
-        // Workaround for netlify issue
-        // https://github.com/nuxt/framework/issues/6204
-        config.build.rollupOptions.output.inlineDynamicImports = true;
-      }
-    },
   },
 });
