9a593507399077da31403f5c81926503312f0159
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2022-09-07 01:08:09 +0530

    chore: add root elem
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- pages/articles/[...article].vue
+++ pages/articles/[...article].vue
@@ -1,21 +1,23 @@
 <template>
-  <article v-if="data" class="flex flex-col w-full h-full prose prose-lg">
-    <h1>{{ data.title }}</h1>
-    <div
-      v-if="data.tags && data.tags.length"
-      class="flex items-center justify-start py-1 text-sm"
-    >
-      <div v-for="(tag, index) in data.tags" :key="index" class="pl-2">
-        <span
-          class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-red-100 text-gray-800 mr-2"
-        >
-          {{ tag }}
-        </span>
+  <section class="flex items-center justify-center w-full h-full">
+    <article v-if="data" class="flex flex-col w-full h-full prose prose-lg">
+      <h1>{{ data.title }}</h1>
+      <div
+        v-if="data.tags && data.tags.length"
+        class="flex items-center justify-start py-1 text-sm"
+      >
+        <div v-for="(tag, index) in data.tags" :key="index" class="pl-2">
+          <span
+            class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-red-100 text-gray-800 mr-2"
+          >
+            {{ tag }}
+          </span>
+        </div>
       </div>
-    </div>
-    <h3>{{ data.description }}</h3>
-    <content-doc />
-  </article>
+      <h3>{{ data.description }}</h3>
+      <content-doc />
+    </article>
+  </section>
 </template>
 <script setup lang="ts">
   const route = useRoute();
