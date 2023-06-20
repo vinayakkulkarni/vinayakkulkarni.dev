53a15e59df0ec080172429fe1d2a346e1f71914c
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-06-20 06:10:15 +0530

    fix: slots issue with articles
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- pages/articles/index.vue
+++ pages/articles/index.vue
@@ -3,9 +3,9 @@
     <!-- Top Data -->
     <div class="grid grid-flow-col grid-cols-4 gap-4">
       <div class="inline-flex flex-col self-center justify-end col-span-3">
-        <p class="text-3xl">Blog Posts</p>
+        <p class="text-3xl">Blog articles</p>
         <p>
-          This page will serve as a listing for all the TIL's, blog posts &
+          This page will serve as a listing for all the TIL's, blog articles &
           learnings.
         </p>
       </div>
@@ -18,38 +18,50 @@
       </nuxt-link>
     </div>
     <!-- Actual Data -->
-    <content-list v-slot="{ list }">
-      <div
-        v-if="list.length > 0"
-        class="grid gap-4 py-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
-      >
+    <content-list path="/articles">
+      <template #default="{ list }">
         <div
-          v-for="post in list"
-          v-show="post.status !== 'draft'"
-          :key="post.position"
-          class="w-full overflow-hidden dark:bg-gray-700 bg-gray-900 text-gray-200 rounded shadow cursor-pointer md:max-w-lg hover:shadow-md"
-          :title="post.title"
-          @click="$router.push({ path: post._path })"
+          v-if="list.length > 0"
+          class="grid gap-4 py-4 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4"
         >
-          <div class="p-4">
-            <div class="mb-2 text-xl font-bold">
-              {{ post.title }}
-            </div>
-            <div class="flex items-center justify-start py-1 text-sm">
-              <div v-for="(tag, index) in post.tags" :key="index" class="pl-2">
-                <span
-                  class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-red-100 text-gray-800 mr-2"
-                >
-                  {{ tag }}
-                </span>
+          <div
+            v-for="article in list"
+            v-show="article.status !== 'draft'"
+            :key="article.position"
+            class="w-full overflow-hidden dark:bg-gray-700 bg-gray-900 text-gray-200 rounded shadow cursor-pointer md:max-w-lg hover:shadow-md"
+            :title="article.title"
+            @click="router.push({ path: article._path })"
+          >
+            <div class="p-4">
+              <div class="mb-2 text-xl font-bold">
+                {{ article.title }}
               </div>
-            </div>
-            <div class="pt-2">
-              {{ post.description }}
+              <div class="flex items-center justify-start py-1 text-sm">
+                <div
+                  v-for="(tag, index) in article.tags"
+                  :key="index"
+                  class="pl-2"
+                >
+                  <span
+                    class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium leading-4 bg-red-100 text-gray-800 mr-2"
+                  >
+                    {{ tag }}
+                  </span>
+                </div>
+              </div>
+              <div class="pt-2">
+                {{ article.description }}
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </template>
+      <template #not-found>
+        <p>No articles found.</p>
+      </template>
     </content-list>
   </section>
 </template>
+<script setup lang="ts">
+  const router = useRouter();
+</script>
