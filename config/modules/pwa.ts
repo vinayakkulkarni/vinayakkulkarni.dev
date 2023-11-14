c2b75e66246774ec9ec555d544d9772853c204b6
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2023-11-14 11:30:10 +0530

    feat(pwa): add pwa module config
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- config/modules/pwa.ts
+++ config/modules/pwa.ts
@@ -1,7 +1,42 @@
 import type { NuxtConfig } from 'nuxt/schema';
 
 export const config: NuxtConfig['pwa'] = {
+  registerType: 'prompt',
+  manifest: {
+    name: 'Vinayak Kulkarni - Portfolio',
+    short_name: 'VK',
+    theme_color: '#ffffff',
+    background_color: '#c3c3c3',
+    display: 'standalone',
+    icons: [
+      {
+        src: 'pwa-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+      {
+        src: 'pwa-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+  },
   workbox: {
-    enabled: process.env.NODE_ENV === 'production',
+    globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+  },
+  client: {
+    installPrompt: true,
+  },
+  devOptions: {
+    enabled: true,
+    suppressWarnings: true,
+    navigateFallbackAllowlist: [/^\/$/],
+    type: 'module',
   },
 };
