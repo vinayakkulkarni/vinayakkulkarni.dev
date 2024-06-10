23b1ad24b29fac84a036cb3b02c477b7f022b2e2
Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>
2024-06-10 16:06:40 +0530

    fix: use 100dvh
    Signed-off-by: Vinayak Kulkarni <19776877+vinayakkulkarni@users.noreply.github.com>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,15 +1,15 @@
 <template>
   <div
-    class="relative h-screen min-w-screen w-screen flex flex-col select-none overflow-hidden bg-stone-100 text-stone-800 font-light transition-colors duration-200 ease-linear dark:bg-stone-900 dark:text-stone-100"
+    class="relative h-100dvh min-w-screen w-screen flex flex-col select-none overflow-hidden bg-stone-100 text-stone-800 font-light transition-colors duration-200 ease-linear dark:bg-stone-900 dark:text-stone-100"
   >
-    <v-spotlight v-if="isDark && isDesktop" />
+    <!-- <v-spotlight v-if="isDark && isDesktop" /> -->
     <!-- Header -->
     <header>
       <v-header />
     </header>
     <!-- Body -->
     <main
-      class="scrollbar-thin scrollbar-track-stone-400 scrollbar-thumb-stone-600 dark:scrollbar-thumb-stone-400 dark:scrollbar-track-stone-600 flex-1 overflow-y-auto p-4"
+      class="scrollbar-track-stone-400 scrollbar-thumb-stone-600 dark:scrollbar-thumb-stone-400 dark:scrollbar-track-stone-600 flex-1 overflow-y-auto p-4 scrollbar-thin"
     >
       <div class="h-full flex items-center justify-center">
         <slot class="md:max-w-screen-xl" />
@@ -25,8 +25,8 @@
 <script setup lang="ts">
   import VFooter from '~/components/common/VFooter.vue';
   import VHeader from '~/components/common/VHeader.vue';
-  import { VSpotlight } from 'v-spotlight';
+  // import { VSpotlight } from 'v-spotlight';
 
-  const isDark = useDark();
-  const { isDesktop } = useDevice();
+  // const isDark = useDark();
+  // const { isDesktop } = useDevice();
 </script>
